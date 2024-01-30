91415fabc82e70448a6993e49c365e1985b5887a
omripfeifer <omri.pfeifer@post.runi.ac.il>
2024-01-30 20:30:59 +0200

    change 1

--- HashTagTokenizer.java
+++ HashTagTokenizer.java
@@ -13,18 +13,35 @@ public class HashTagTokenizer {
 		String[] dictionary = new String[3000];
 
 		In in = new In(fileName);
+		int numberOfWord = 0 ; 
 
-		// Your code here
+		while (!in.isEmpty() && numberOfWord < 3000 ){
+			
+			String word = in.readString() ;
+			dictionary[numberOfWord] = word ; 
+			numberOfWord++ ; 
+
+		}
+		in.close(); 
 
 		return dictionary;
 	}
 
 	public static boolean existInDictionary(String word, String []dictionary) {
-		// Your code here
+		
+		boolean isInDictionary = false ;
+		for (int i = 0 ; i < 3000 ; i++ ){
+
+		if ( word.equals(dictionary[i]) ){
+			isInDictionary = true ; 
+			break ; // ending loop 
+		}
+	  }	
+	  return isInDictionary ; 
 	}
 
 	public static void breakHashTag(String hashtag, String[] dictionary) {
-
+		String lowerCaseHashTag = hashtag.toLowerCase();
 		// Base case: do nothing (return) if hashtag is an empty string.
         if (hashtag.isEmpty()) {
             return;
@@ -33,8 +50,14 @@ public class HashTagTokenizer {
         int N = hashtag.length();
 
         for (int i = 1; i <= N; i++) {
-		
-        }
+
+			if ( existInDictionary(lowerCaseHashTag.substring(0,i), dictionary)) {
+
+				System.out.println(lowerCaseHashTag.substring(0,i) ) ; 
+				breakHashTag(lowerCaseHashTag.substring(i), dictionary);
+				return ; 
+			} 
     }
 
+  }
 }
